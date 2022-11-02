61417bfa3592fc29ac7faaeeae8526f5b2029c97
AmirBashan <amir_bashan@webiks.com>
2022-11-02 09:52:56 +0200

    rutes added

---
+++ Server/src/route/bunnies.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response,Router  } from "express";
+export const bunnyRoute = express.Router();
+
+bunnyRoute.get('/', (req: Request, res: Response) => {
+    res.send([{
+        id: 0,
+        name: "Snowball",
+        cuteness: 10,
+        color: "white",
+      }]);
+})
+
--- Server/src/server.ts
+++ Server/src/server.ts
@@ -1,16 +1,10 @@
-import express, { Application, Request, Response } from "express";
-const app: Application = express()
+import express, { Application, Request, Response,Router  } from "express";
+import {bunnyRoute} from "./route/bunnies"
 
+const app: Application = express()
 const port: number = 8000
 
-app.get('/bunnies', (req: Request, res: Response) => {
-    res.send([{
-        id: 0,
-        name: "Snowball",
-        cuteness: 10,
-        color: "white",
-      }]);
-})
+app.use("/bunnies", bunnyRoute);
 
 app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
